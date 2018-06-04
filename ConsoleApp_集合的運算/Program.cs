e3d0162e050cbb6445ca327ecb9692dbc9ed8deb
joe lee <as955133@gmail.com>
2018-06-04 14:57:01 +0800

    加入專案檔案。

---
+++ ConsoleApp_集合的運算/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_集合的運算
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
