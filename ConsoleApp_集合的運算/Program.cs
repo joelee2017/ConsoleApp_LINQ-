0b2505e869f935db15feb4da60b48454a7b4cf1d
joe lee <as955133@gmail.com>
2018-06-04 15:09:45 +0800

    Union/Intersect/Except/Distinct

--- ConsoleApp_集合的運算/Program.cs
+++ ConsoleApp_集合的運算/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ConsoleApp_集合的運算
 {
@@ -6,7 +7,51 @@ namespace ConsoleApp_集合的運算
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Union/Intersect/Except
+            int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            int[] numbersB = { 1, 3, 5, 7, 8 };
+
+            var unionResults = numbersA.Union(numbersB);
+            var intersectResults = numbersA.Intersect(numbersB);
+            var exceptResults = numbersA.Except(numbersB);
+
+            Console.WriteLine("== Union ==");
+
+            foreach(var q in unionResults)
+            {
+                Console.Write(q + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("== Intersect ==");
+
+            foreach(var q in intersectResults)
+            {
+                Console.Write(q + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("== Except ==");
+
+            foreach(var q in exceptResults)
+            {
+                Console.Write(q + " ");
+            }
+
+            Console.WriteLine();
+
+            // distinct
+            int[] numberSeries = { 2, 2, 3, 5, 5 };
+            var distinctValues = numberSeries.Distinct();
+
+            Console.WriteLine("Distinc values from numberSeries");
+
+            foreach( var q in distinctValues)
+            {
+                Console.Write(q + " ");
+            }
+
+            Console.Read();
         }
     }
 }
